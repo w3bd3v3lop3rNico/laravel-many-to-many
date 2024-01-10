bf6c5af5b9cb9cf1d99dc4891aa31bc6fe82e2a6
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-10 16:54:20 +0100

    Fixed migrations and create technologies and pivot table

--- database/migrations/2024_01_10_144823_create_project_technology_table.php
+++ database/migrations/2024_01_10_144823_create_project_technology_table.php
@@ -9,18 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('technology_id');
+
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+            $table->foreign('technology_id')->references('id')->on('technologies')->cascadeOnDelete();
+
+            $table->primary(['project_id', 'technology_id']);
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('project_technology');
     }
