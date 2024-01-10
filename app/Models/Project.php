6ab8b51224626ac182b8a9be38f0a9059e974071
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-10 17:24:28 +0100

    Add reletions many to many in project and technology models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -20,4 +20,8 @@ class Project extends Model
     {
         return $this->belongsTo(Category::class);
     }
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 }
