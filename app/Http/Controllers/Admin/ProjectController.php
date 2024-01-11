255c4605f71954835058af6f7cbcf7d05dcc1226
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-11 11:01:59 +0100

    Fix Destroy with techs sync

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -117,6 +117,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        
+        $project->technologies()->sync([]);
         $project->delete();
 
         return redirect()->route('admin.projects.index');
